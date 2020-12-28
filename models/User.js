36267031429fd9c62b71ab51e5fad2d72bcc29ef
“delrod” <thedavidelrod@gmail.com>
2020-12-28 09:38:35 -0900

    made user model

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+username: {
+    type: String,
+    required: true
+},
+email: {
+    type: String,
+    required: true
+},
+password: {
+    type: String,
+    required: true
+},
+performerType: {
+type: String,
+required: false
+},
+bio: {
+   type: Text,
+   required: false 
+},
+imageURL: {
+    type: String,
+    required: false
+},
+venues: {
+    type: Schema.Types.ObjectId,
+    ref: "venues"
+},
+comments: {
+    type: Schema.Types.ObjectId,
+    ref: "comments"
+},
+ratings: {
+    type: Schema.Types.ObjectId,
+    ref: "ratings"
+}
+
+
+
+})
