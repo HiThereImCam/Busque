a906a3e31b39466ab21e0b4c550c31caebf83218
“delrod” <thedavidelrod@gmail.com>
2020-12-28 11:09:20 -0900

    progress on user auth

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://dev:kQuYSGozYRdYpx7d@busque.on67c.mongodb.net/busque?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
+ const keys = require("../../config/keys");
 const passport = require("passport");
 
-router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+router.get("/test", (req, res) => res.json({ msg: "This is the users route ya bish" }));
+
+
+
+module.exports = router;
