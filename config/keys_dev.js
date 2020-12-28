d6f01e1ba90158b38bd5505b7e7b90e3159c0ecf
HiThereImCam <ctanjoco@gmail.com>
2020-12-28 12:28:49 -0800

    Delete keys_dev.js
    Added our secrets........ 😢

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:kQuYSGozYRdYpx7d@busque.on67c.mongodb.net/busque?retryWrites=true&w=majority",
-  secretOrKey: "Pj~{>?r>iG)(jgl+&tgqP|)11oTZqZ",
-};
