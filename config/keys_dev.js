d4f95158a736e4c139a98519e8423f8b0b549aee
“delrod” <thedavidelrod@gmail.com>
2020-12-29 13:21:07 -0900

    Untrack files in .gitignore

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://dev:YXMd5cpyZDxzPmIy@busque.on67c.mongodb.net/Busque?retryWrites=true&w=majority",
-  secretOrKey: "Pj~{>?r>iG)(jgl+&tgqP|)11oTZqZ",
-};
-//* why does this keep going to git?
