43e898a8b689772315bf38a9a8e118e553252b76
“delrod” <thedavidelrod@gmail.com>
2020-12-29 21:11:55 -0900

    fixed gitignore

--- routes/api/venues.js
+++ routes/api/venues.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const passport = require("passport");
 const mongoose = require("mongoose");
 const Venue = require("../../models/Venue");
+const validateVenueInput = require("../../validations/venue");
 
 router.get("/test", (req, res) =>
   res.json({ msg: "This is the venue route ya bish" })
@@ -20,18 +21,25 @@ router.post( //create venue
   "/",
   passport.authenticate('jwt', { session: false }),
   (req, res) => {
+
+    const { errors, isValid } = validateVenueInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors); //union square long-122.4045 lat37.78616 long should be first
+    }
+
     const newVenue = new Venue({
       name: req.body.name,
       longitude: req.body.longitude,
       latitude: req.body.latitude,
       type: req.body.type,
       available: req.body.available,
+      user: req.user.id,
     });
 
     newVenue.save().then((venue) => res.json(venue));
   }
 );
 
-//union square long-122.4045 lat37.78616 long should be first
 
 module.exports = router;
--- validations/venue.js
+++ validations/venue.js
@@ -4,7 +4,15 @@ const validText = require("./valid-text");
 module.exports = function validateVenueInput(data) {
     let errors = {};
 
+    data.name = validText(data.name) ? data.name : '';
     data.type = validText(data.type) ? data.type : '';
+    data.latitude = validText(data.latitude) ? data.latitude : '';cd 
+    data.longitude = validText(data.longitude) ? data.longitude : '';
+    data.available = validText(data.available) ? data.available : '';
+
+    if (Validator.isEmpty(data.name)) {
+        errors.name = "Name field is required";
+    }
 
     if (!Validator.isDecimal(data.latitude)) {
         errors.latitude = "Coordinates for latitude is required"
@@ -18,9 +26,9 @@ module.exports = function validateVenueInput(data) {
         errors.type = "Text field is required";
     }
 
-    if (!Validator.isBoolean(data.available)) {
-        errors.available = "A venue must be either available or not available"
-    }
+    // if (!Validator.isBoolean(data.available)) {
+    //     errors.available = "A venue must be either available or not available"
+    // }
 
     return {
         errors,
