5ff0c2852a06f73e12e231a1baf7d1b7bcda756f
alanashannon <alashannon@gmail.com>
2020-12-30 17:16:17 -0800

    Edit main page component and styling

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
 import Mapbox from "../mapbox/mapbox";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import "../../css/header.css";
+
 
 class MainPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      open: false
+    }
+
+    this.openSearchModal = this.openSearchModal.bind(this);
+    this.closeSearchModal = this.closeSearchModal.bind(this); 
+  }
+
+  openSearchModal() {
+    this.setState({ open: true })
+    console.log("hello")
+  }
+
+  closeSearchModal() {
+    this.setState({ open: false })
   }
 
   render() {
     return (
       <div>
+        <header className="header">
+          <div className="header-left">
+            <div className="hamburger">
+              <FontAwesomeIcon onClick={this.openSearchModal} className="fas faBars fa-2x" icon={faBars} />
+            </div>
+            <div className="search-bar">
+              <input type="text" placeholder="Search" />
+              <FontAwesomeIcon className="search-icon" icon={faSearch} />
+            </div>
+          </div>
+          <div className="header-right">
+            <Link className="login-button" to={"/login"}>Login</Link>
+          </div>
+        </header>
         <Mapbox />
       </div>
     );
@@ -16,3 +50,4 @@ class MainPage extends Component {
 }
 
 export default MainPage;
+
