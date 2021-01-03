2591e846a2721b9d356e87245b890b6f4ed5db40
Cameron Tanjoco <ctanjoco@gmail.com>
2021-01-02 16:27:56 -0800

    Added nav modal button

---
+++ frontend/src/components/navigation/nav_modal.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "../../css/nav_modal.css";
+class NavModal extends Component {
+  render() {
+    return <div className="nav-container">hello from container</div>;
+  }
+}
+
+export default NavModal;
