29cefcd27d66477f8ee532d4c99c43802268eb97
“delrod” <thedavidelrod@gmail.com>
2021-01-03 16:26:09 -0900

    clean rep for pull

--- frontend/src/components/main/main_page_container.js
+++ frontend/src/components/main/main_page_container.js
@@ -1,10 +1,10 @@
 import { connect } from "react-redux";
 import MainPage from "./main_page";
 
-// const mapStateToProps = (state) => ({});
+const mapStateToProps = (state) => ({});
 
-// const mapDispatchToProps = (dispatch) => ({
-//   closeNavModal: () => dispatch(closeNavModal()),
-// });
+const mapDispatchToProps = (dispatch) => ({
+  closeNavModal: () => dispatch(closeNavModal()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
