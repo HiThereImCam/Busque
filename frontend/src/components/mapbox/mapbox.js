562aa3b7dc66b000a67a1e4404d16f6264e624bf
Cameron Tanjoco <ctanjoco@gmail.com>
2021-01-12 19:13:09 -0800

    Added popup to every marker

--- frontend/src/components/mapbox/mapbox.js
+++ frontend/src/components/mapbox/mapbox.js
@@ -1,9 +1,10 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import mapboxgl from "mapbox-gl";
 import "../../css/mapbox.css";
 
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
+import defaultMarkers from "../../config/defaultMarkers";
 
 const REACT_APP_MAPBOX_KEY = process.env.REACT_APP_MAPBOX_KEY;
 
@@ -16,13 +17,14 @@ class MapBox extends Component {
       lng: -122.4363143,
       lat: 37.7461108,
       zoom: 12,
+      markerColor: "#4CBB17",
     };
     this.marker = new mapboxgl.Marker();
+    this.mapBoxRef = React.createRef();
+    this.buttonRef = React.createRef();
+    this.handleClick = this.handleClick.bind(this);
+    window.handleClick = this.handleClick;
   }
-
-  /**
-   * Possibly create util
-   */
 
   componentDidMount() {
     if (window.matchMedia("(max-width: 420px)")) {
@@ -31,6 +33,7 @@ class MapBox extends Component {
         lat: 37.5713,
       });
     }
+
 
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
@@ -46,14 +49,6 @@ class MapBox extends Component {
     });
     this.map.addControl(geocoder, "top-left");
 
-    this.map.on("click", (e) => {
-      this.marker.remove();
-      e.preventDefault();
-      let { lng, lat } = e.lngLat;
-      console.log([lng, lat]);
-      this.marker.setLngLat([lng, lat]).addTo(this.map);
-    });
-
     this.map.on("move", () => {
       this.setState({
         lng: this.map.getCenter().lng.toFixed(4),
@@ -61,11 +56,34 @@ class MapBox extends Component {
         zoom: this.map.getZoom().toFixed(2),
       });
     });
+
+    defaultMarkers.coordinates.forEach((coordinate) => {
+      this.marker = new mapboxgl.Marker({
+        color: this.state.markerColor,
+      })
+        .setLngLat(coordinate)
+        .addTo(this.map);
+      this.marker
+        .setPopup(
+          new mapboxgl.Popup().setLngLat(coordinate).setHTML(
+            `
+              <button id="checkIn" onclick="handleClick()" ref=${this.buttonRef.current}>Check in</button>
+            `
+          )
+        )
+        .addTo(this.map);
+    });
   }
+
+  handleClick = () => {
+    let svg = document.getElementsByTagName("svg");
+  };
 
   render() {
     return (
-      <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+      <Fragment>
+        <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+      </Fragment>
     );
   }
 }
