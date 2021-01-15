67322ef4f1bde6847c4b933bea34aca582f357c8
“delrod” <thedavidelrod@gmail.com>
2021-01-14 16:02:45 -0900

    add currentUser array to Venue Schema

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const fs = require("fs");
+const http = require("http");
+const aws = require("aws-sdk");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
-var _ = require("lodash");
 const validateRegisterInput = require("../../validations/register");
 const validateLoginInput = require("../../validations/login");
+
+
 
 
 router.get("/", (req, res) => {
