373013cbb224a1c20e05276220138de23fee29f7
alanashannon <alashannon@gmail.com>
2021-01-20 12:48:26 -0800

    Try fixing user show

--- frontend/src/components/user/user_show.js
+++ frontend/src/components/user/user_show.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 class UserShow extends React.Component {
   componentDidMount() {
@@ -14,11 +15,32 @@ class UserShow extends React.Component {
 
       return (
         <div>
-          <h1>hello</h1>
-          <img src={user.imageURL} alt="profile" />
-          <div>{user.username}</div>
-          <div>{user.performerType}</div>
-          <div>{user.bio}</div>
+          <div className="user-header">
+            <Link to="/" >
+              <h1 className="user-header-h1">Busque</h1>
+            </Link>
+          </div>
+          <div className="user-show-info-list">
+            <div className="user-show-photo-outer">
+              <div className="user-show-photo-inner">
+                <img src={user.imageURL} alt="profile" />
+              </div>
+            </div>
+            <div className="user-show-info">
+              <div className="user-show-username">
+                {user.username}
+              </div>
+              <div className="user-show-performer-type">
+                Performer Type: {user.performerType}
+              </div>
+              <div className="user-show-bio">
+                Bio: {user.bio}
+              </div>
+            </div>
+          </div>
+          <div className="user-reviews">
+            <h2>Reviews</h2>
+          </div>
         </div>
       );
     }
