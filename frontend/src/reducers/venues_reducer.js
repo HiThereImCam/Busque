f1ea0873603d9333d94e760e26c1df86cf87cdc8
alanashannon <alashannon@gmail.com>
2021-01-22 18:30:34 -0800

    Add comment stuff

--- frontend/src/reducers/venues_reducer.js
+++ frontend/src/reducers/venues_reducer.js
@@ -1,4 +1,4 @@
-import { CHECK_IN, RECEIVE_VENUES } from "../actions/venue_actions";
+import { CHECK_IN, RECEIVE_VENUES, RECEIVE_COMMENT } from "../actions/venue_actions";
 import { formatVenues } from "./venue_selectors";
 const VenuesReducer = (state = [], action) => {
   Object.freeze(state);
@@ -10,6 +10,11 @@ const VenuesReducer = (state = [], action) => {
     case CHECK_IN:
       // return Object.assign({}, state, { [action.data.id]: action.data });
       console.log(state.venues);
+    // case RECEIVE_COMMENT: 
+    //   return action.comment.data.comments
+      // for (let i = 0; i < action.comment.data.comments.length; i++) {
+      //   return Object.assign({}, state, { [action.comment.data.comments[i]]: action.comment.data.comments[i].comment })
+      // }
     default:
       return state;
   }
