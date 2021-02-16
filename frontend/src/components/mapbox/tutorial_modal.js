02dffae4614684040e7bdfe5b288475046cc8dbb
Cameron Tanjoco <ctanjoco@gmail.com>
2021-02-15 16:38:44 -0800

    prepping to move branches

--- frontend/src/components/mapbox/tutorial_modal.js
+++ frontend/src/components/mapbox/tutorial_modal.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 class Tutorial extends Component {
   constructor(props) {
@@ -12,6 +13,8 @@ class Tutorial extends Component {
   render() {
     return (
       <div>
+        <FaArrowAltCircleLeft className="left-arrow" />
+        <FaArrowAltCircleLeft className="right-arrow" />
         <button type="checkbox" onClick={this.handleClick}>
           Don't show again
         </button>
