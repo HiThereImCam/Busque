081a2cfa8aa643fcbfac650ca9909da85fa6a96c
alanashannon <alashannon@gmail.com>
2021-02-17 11:53:06 -0800

    Prep for pull

---
+++ frontend/src/util/like_api_util.js
@@ -0,0 +1,21 @@
+import axios from 'axios'; 
+
+export const getLikes = () => {
+    return axios.get('/api/likes/')
+}
+
+export const getUserLikes = (userId) => {
+    return axios.get(`/api/likes/user/${userId}`)
+}
+
+export const getVenueLikes = (venueId) => {
+    return axios.get(`/api/likes/venue/${venueId}`)
+}
+
+export const createLike = like => {
+    return axios.post(`/api/likes/new`, like)
+}
+
+export const deleteLike = likeId => {
+    return axios.delete(`/api/likes/${likeId}`)
+}
